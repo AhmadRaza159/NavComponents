645fd7b174619f36b2e15fb25ce63763fed872d1
Ahmad <ahmadrazaofficial159@gmail.com>
2023-03-16 23:43:38 +0500

    creating fragments in nav graph

---
+++ app/src/main/java/com/example/navcomponents/FragmentViewAllCrime.kt
@@ -0,0 +1,7 @@
+package com.example.navcomponents
+
+import androidx.fragment.app.Fragment
+
+class FragmentViewAllCrime : Fragment(R.layout.fragment_view_all_crime) {
+
+}
---
+++ app/src/main/java/com/example/navcomponents/RegisterFragmentPersonalDetails.kt
@@ -0,0 +1,11 @@
+package com.example.navcomponents
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class RegisterFragmentPersonalDetails : Fragment(R.layout.fragment_register_personal_info) {
+
+}
