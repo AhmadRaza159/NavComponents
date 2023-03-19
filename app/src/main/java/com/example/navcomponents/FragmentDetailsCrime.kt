1daaf491615fee9a3fb28069000fcdb8d1d4666a
Ahmad <ahmadrazaofficial159@gmail.com>
2023-03-20 00:47:52 +0500

    create nav graph

---
+++ app/src/main/java/com/example/navcomponents/FragmentDetailsCrime.kt
@@ -0,0 +1,7 @@
+package com.example.navcomponents
+
+import androidx.fragment.app.Fragment
+
+class FragmentDetailsCrime : Fragment(R.layout.fragment_details_crime) {
+
+}
---
+++ app/src/main/java/com/example/navcomponents/FragmentFindACrime.kt
@@ -0,0 +1,7 @@
+package com.example.navcomponents
+
+import androidx.fragment.app.Fragment
+
+class FragmentFindACrime : Fragment(R.layout.fragment_find_crime) {
+
+}
