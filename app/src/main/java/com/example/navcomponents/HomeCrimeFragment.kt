351dd3cd409c04aec9c32ad9f3bcf7f565db3bc4
Ahmad <ahmadrazaofficial159@gmail.com>
2023-03-23 18:10:38 +0500

    working on actions

--- app/src/main/java/com/example/navcomponents/HomeCrimeFragment.kt
+++ app/src/main/java/com/example/navcomponents/HomeCrimeFragment.kt
@@ -5,11 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import com.example.navcomponents.databinding.FragmentHomeCrimeBinding
 
 class HomeCrimeFragment : Fragment(R.layout.fragment_home_crime) {
+    private lateinit var binding:FragmentHomeCrimeBinding
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding=FragmentHomeCrimeBinding.bind(view)
+        val navController=binding.root.findNavController()
 
+        binding.btnRegisterNewCrime.setOnClickListener{
+            navController.navigate(HomeCrimeFragmentDirections.actionHomeCrimeFragmentToRegisterFragmentPersonalDetails())
+        }
+        binding.btnFindACrime.setOnClickListener{
+            navController.navigate(HomeCrimeFragmentDirections.actionHomeCrimeFragmentToFragmentFindACrime())
+        }
+        binding.btnViewAllCrimes.setOnClickListener {
+            navController.navigate(HomeCrimeFragmentDirections.actionHomeCrimeFragmentToFragmentViewAllCrime())
+        }
     }
 
 }
