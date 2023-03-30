1471fd02eba60fe20e0f90332746351ef10f4d01
Ahmad <ahmadrazaofficial159@gmail.com>
2023-03-30 23:38:40 +0500

    safeargs and popup

--- app/src/main/java/com/example/navcomponents/RegisterFragmentCrimeDetails.kt
+++ app/src/main/java/com/example/navcomponents/RegisterFragmentCrimeDetails.kt
@@ -5,9 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.navArgs
+import com.example.navcomponents.databinding.FragmentRegisterCrimeDetailsBinding
 
-class RegisterFragmentCrimeDetails : Fragment(R.layout.fragment_register_personal_info) {
+class RegisterFragmentCrimeDetails : Fragment(R.layout.fragment_register_crime_details) {
+    private lateinit var binding:FragmentRegisterCrimeDetailsBinding
+    private val args : RegisterFragmentCrimeDetailsArgs by navArgs()
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding= FragmentRegisterCrimeDetailsBinding.bind(view)
+
+        binding.textViewName.text="${args.name}'s case"
+
+        binding.btnCancel.setOnClickListener {
+            binding.root.findNavController().popBackStack(R.id.homeCrimeFragment,false)
+        }
+
     }
 }
--- app/src/main/java/com/example/navcomponents/RegisterFragmentPersonalDetails.kt
+++ app/src/main/java/com/example/navcomponents/RegisterFragmentPersonalDetails.kt
@@ -5,9 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import com.example.navcomponents.databinding.FragmentRegisterPersonalInfoBinding
 
 class RegisterFragmentPersonalDetails : Fragment(R.layout.fragment_register_personal_info) {
+    private lateinit var binding:FragmentRegisterPersonalInfoBinding
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding= FragmentRegisterPersonalInfoBinding.bind(view)
+
+        binding.btnNext.setOnClickListener {
+            binding.root.findNavController().navigate(RegisterFragmentPersonalDetailsDirections.actionRegisterFragmentPersonalDetailsToRegisterFragmentCrimeDetails(binding.etName.text.toString(), binding.etAge.text.toString(), binding.etPhone.text.toString()))
+        }
+
+        binding.btnCancel.setOnClickListener {
+            binding.root.findNavController().popBackStack()
+        }
     }
 }
