8af470e263832b2447cfb4f6fb0615101473c7cb
Ahmad <ahmadrazaofficial159@gmail.com>
2023-04-04 23:04:34 +0500

    NavigationUi

--- app/src/main/java/com/example/navcomponents/MainActivity.kt
+++ app/src/main/java/com/example/navcomponents/MainActivity.kt
@@ -2,13 +2,25 @@ package com.example.navcomponents
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.navcomponents.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController:NavController
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
         //Crime Report Application using Navigation Components
+        navController=(supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment).navController
+        setupActionBarWithNavController(navController)
+    }
 
-
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
